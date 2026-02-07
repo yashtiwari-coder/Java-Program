a34b31b0435726dd2bb0014d03b922b80ed22218
yashtiwari-coder <yashsharma78198@gmail.com>
2026-02-07 23:05:49 +0530

    Menu_driven_CALCULATOR

---
+++ Menu_driven_calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Menu_driven_calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.println("\n-----Calculator Menu-----");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("Choose an operation (1-4): ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid operation.");
+        }
+        sc.close();
+    }
+}
