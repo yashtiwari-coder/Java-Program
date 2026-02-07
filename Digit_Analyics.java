b6a3898148df49d13656a19e72844551584e2550
yashtiwari-coder <yashsharma78198@gmail.com>
2026-02-08 00:17:45 +0530

    Digit_Analyics

--- Digit_Analyics.java
+++ Digit_Analyics.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Digit_Analyics{
     public static void main(String [ ] args){
-        Scanner sc= new scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
         int original = number;
